ebce14ff8a6a36da4ffcc1cf2ff901a8c901d84f
Piotr Justyna <piotr.justyna@gmail.com>
2019-05-10 09:09:13 +0100

    Problem 27, C#.

---
+++ 27/C#/Problem27/Coefficients.cs
@@ -0,0 +1,14 @@
+public class Coefficients
+{
+    public Coefficients(
+        int b,
+        int c)
+        {
+            B = b;
+            C = c;
+        }
+
+    public int B { get; }
+
+    public int C { get; }
+}
