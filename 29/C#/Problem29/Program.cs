faccae351cb5868f784d0078cf43a338e35583b6
Piotr Justyna <piotr.justyna@gmail.com>
2019-05-14 12:03:31 +0100

    Problem 29, C#.

---
+++ 29/C#/Problem29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem29
+{
+    class Program
+    {
+        private const int MinA = 2;
+
+        private const int MaxA = 100;
+
+        private const int MinB = 2;
+
+        private const int MaxB = 100;
+
+        private static Dictionary<Double, Tuple<int, int>> _distinctTerms = new Dictionary<double, Tuple<int, int>>(); 
+
+        static void Main(string[] args)
+        {
+            for (int a = MinA; a <= MaxA; a++)
+            {
+                for (int b = MinB; b <= MaxB; b++)
+                {
+                    var power = Math.Pow(a, b);
+
+                    if(!_distinctTerms.ContainsKey(power))
+                    {
+                        _distinctTerms.Add(power, new Tuple<int, int>(a, b));
+                    }
+                }
+            }
+
+            Console.WriteLine($"There are {_distinctTerms.Count} distinct terms.");
+        }
+    }
+}
