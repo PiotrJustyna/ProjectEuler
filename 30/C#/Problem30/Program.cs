bf90215dd352abb3a7b1d6aa84ddd15bbee990ae
Piotr Justyna <piotr.justyna@gmail.com>
2019-05-15 16:20:00 +0100

    Problem 30, C#. Solution explanation.

--- 30/C#/Problem30/Program.cs
+++ 30/C#/Problem30/Program.cs
@@ -32,6 +32,16 @@ namespace Problem30
             // This is the first iteration of the solution.
             // It works, but the "6" is hardcoded.
             // Thinking of a way to improve it.
+            // ###
+            // Edit:
+            // Ah, it makes sense if you think of it:
+            // if you form a number consisting of 5 nines (5 * (9 ^ 5)), you get 295,245 (6 digits)
+            // going further:
+            // 6 * (9 ^ 5) = 354,294 (6 digits)
+            // 7 * (9 ^ 5) = 413,343 (still only 6 digits)
+            // Result: 6 digits is the maximum length of a sum that can still match the length of the potential number.
+            // From that point on, the length of potential numbers grows faster that the sums of even the highest available powers (9 ^ 5).
+            // Result: 6 is a reasonable ceiling for the problem.
             var potentialNumbers = GenerateNumbers(_powers, 6);
 
             foreach (var singlePotentialNumber in potentialNumbers)
